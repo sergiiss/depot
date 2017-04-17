3e5c852cae6c4298e8bfd66cc267be521e7f7d08
Sergiiss <Dracula23@mail.ru>
2017-04-17 22:15:08 +0300

    Pages 224

--- test/integration/user_stories_test.rb
+++ test/integration/user_stories_test.rb
@@ -44,11 +44,11 @@ class UserStoriesTest < ActionDispatch::IntegrationTest
     assert_equal "Check", order.pay_type
 
     assert_equal 1, order.line_items.size
-    line_item = order.line_item[0]
+    line_item = order.line_items[0]
     assert_equal ruby_book, line_item.product
 
     mail = ActionMailer::Base.deliveries.last
-    assert_equal["dave@example.com"], mail.to
+    assert_equal ["dave@example.com"], mail.to
     assert_equal 'Sam Ruby <depot@example.com>', mail[:from].value
     assert_equal "Pragmatic Store Order Confirmation", mail.subject
   end
